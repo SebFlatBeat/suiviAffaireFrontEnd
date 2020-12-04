cf04fa091b2aee23de8ffe46332405e453bb70e2
SebFlatBeat <sebastien.darre@gmail.com>
2020-12-04 17:26:12 +0100

    Ajout du component analyse

---
+++ src/app/analyse/analyse.component.spec.ts
@@ -0,0 +1,25 @@
+import { ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { AnalyseComponent } from './analyse.component';
+
+describe('AnalyseComponent', () => {
+  let component: AnalyseComponent;
+  let fixture: ComponentFixture<AnalyseComponent>;
+
+  beforeEach(async () => {
+    await TestBed.configureTestingModule({
+      declarations: [ AnalyseComponent ]
+    })
+    .compileComponents();
+  });
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(AnalyseComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
