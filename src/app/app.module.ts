16c58b16cce2b6d2081869116f39a50d31e27ac9
SebFlatBeat <sebastien.darre@gmail.com>
2020-12-04 17:30:37 +0100

    Ajout d'AnalyseComponent

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { NgModule } from '@angular/core';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { SyntheseComponent } from './synthese/synthese.component';
+import { AnalyseComponent } from './analyse/analyse.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    SyntheseComponent
+    SyntheseComponent,
+    AnalyseComponent
   ],
   imports: [
     BrowserModule,
