774d2001a5a5da8d7a97a844a5d48d5abe9481a5
SebFlatBeat <sebastien.darre@gmail.com>
2020-12-08 09:08:38 +0100

    Création d'appareil service

---
+++ src/app/analyse.service.ts
@@ -0,0 +1,20 @@
+import { Injectable } from '@angular/core';
+import {HttpClient} from '@angular/common/http';
+import {Observable} from 'rxjs';
+import { environment } from '../environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class AnalyseService {
+
+private apiUrl = environment.apiUrl;
+
+  constructor(private http: HttpClient) {
+  }
+
+  getAnalyse(): Observable<any[]>{
+
+    return this.http.get<any[]>(this.apiUrl + '/analyse');
+  }
+}
