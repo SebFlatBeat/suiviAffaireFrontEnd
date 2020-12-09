23d19d4b554e1db46ea7fcbb728568d9bf88b011
SebFlatBeat <sebastien.darre@gmail.com>
2020-12-09 13:55:45 +0100

    Deplacement dans le package services

--- src/app/services/analyse.service.ts
+++ src/app/services/analyse.service.ts
@@ -1,7 +1,8 @@
 import { Injectable } from '@angular/core';
 import {HttpClient} from '@angular/common/http';
 import {Observable} from 'rxjs';
-import { environment } from '../environments/environment';
+import { environment } from '../../environments/environment';
+import {Analyse} from '../interfaces/analyse';
 
 @Injectable({
   providedIn: 'root'
@@ -13,8 +14,8 @@ private apiUrl = environment.apiUrl;
   constructor(private http: HttpClient) {
   }
 
-  getAnalyse(): Observable<any[]>{
+  getAnalyse(): Observable<Analyse[]>{
 
-    return this.http.get<any[]>(this.apiUrl + '/analyse');
+    return this.http.get<Analyse[]>(this.apiUrl + '/analyse');
   }
 }
